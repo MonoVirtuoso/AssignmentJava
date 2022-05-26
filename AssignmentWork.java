578f884ded1982c6f6c016c9cae80340124153e2
MonoVirtuoso <muhammadaqheel@gmail.com>
2022-05-26 17:31:18 +0800

    Skeleton Function 4,5,6

---
+++ AssignmentWork.java
@@ -0,0 +1,226 @@
+
+package assignmentwork;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class AssignmentWork {
+    
+    public int FunFact(){
+        Random rand = new Random();
+        
+        int randFact = rand.nextInt(10)+1;
+        switch (randFact){
+            case 1:
+                
+            
+        }
+            
+        
+    }
+
+    public int Quiz(int score) {
+        score = 0;
+        Scanner userInput = new Scanner (System.in);
+        System.out.println("Welcome to Green21's Climate Change Quiz!");
+        System.out.println("We'll be giving you a series of question to test your knoweldge on Climate Change and "
+                + "assign you a score based on your answers!");
+        System.out.println("So let's begin!");
+        
+        int answer1 = userInput.nextInt();
+        
+        switch (answer1){
+            case 1: 
+                System.out.println("Wrong");
+            case 2:
+                System.out.println("Wrong");
+            case 3: 
+                System.out.println("Correct!");
+                score += 1;
+            case 4: 
+                System.out.println("Wrong");
+        }
+        
+        int answer2 = userInput.nextInt();
+        
+        switch (answer2){
+            case 1: 
+                System.out.println("Wrong");
+            case 2:
+                System.out.println("Wrong");
+            case 3: 
+                System.out.println("Correct!");
+                score += 1;
+            case 4: 
+                System.out.println("Wrong");
+        }
+        
+        int answer3 = userInput.nextInt();
+        
+        switch (answer3){
+            case 1: 
+                System.out.println("Wrong");
+            case 2:
+                System.out.println("Wrong");
+            case 3: 
+                System.out.println("Correct!");
+                score += 1;
+            case 4: 
+                System.out.println("Wrong");
+        }
+        
+        int answer4 = userInput.nextInt();
+        
+        switch (answer4){
+            case 1: 
+                System.out.println("Wrong");
+            case 2:
+                System.out.println("Wrong");
+            case 3: 
+                System.out.println("Correct!");
+                score += 1;
+            case 4: 
+                System.out.println("Wrong");
+        }
+        
+        int answer5 = userInput.nextInt();
+        
+        switch (answer5){
+            case 1: 
+                System.out.println("Wrong");
+            case 2:
+                System.out.println("Wrong");
+            case 3: 
+                System.out.println("Correct!");
+                score += 1;
+            case 4: 
+                System.out.println("Wrong");
+        }
+        
+        int answer6 = userInput.nextInt();
+        
+        switch (answer6){
+            case 1: 
+                System.out.println("Wrong");
+            case 2:
+                System.out.println("Wrong");
+            case 3: 
+                System.out.println("Correct!");
+                score += 1;
+            case 4: 
+                System.out.println("Wrong");
+        }
+        
+        int answer7 = userInput.nextInt();
+        
+        switch (answer7){
+            case 1: 
+                System.out.println("Wrong");
+            case 2:
+                System.out.println("Wrong");
+            case 3: 
+                System.out.println("Correct!");
+                score += 1;
+            case 4: 
+                System.out.println("Wrong");
+        }
+        
+        int answer8 = userInput.nextInt();
+        
+        switch (answer8){
+            case 1: 
+                System.out.println("Wrong");
+            case 2:
+                System.out.println("Wrong");
+            case 3: 
+                System.out.println("Correct!");
+                score += 1;++
+            case 4: 
+                System.out.println("Wrong");
+        }
+        
+        int answer9 = userInput.nextInt();
+        
+        switch (answer9){
+            case 1: 
+                System.out.println("Wrong");
+            case 2:
+                System.out.println("Wrong");
+            case 3: 
+                System.out.println("Correct!");
+                score += 1;
+            case 4: 
+                System.out.println("Wrong");
+        }
+        
+        int answer10 = userInput.nextInt();
+        
+        switch (answer10){
+            case 1: 
+                System.out.println("Wrong");
+            case 2:
+                System.out.println("Wrong");
+            case 3: 
+                System.out.println("Correct!");
+                score += 1;
+            case 4: 
+                System.out.println("Wrong");
+        }
+        
+        return score;          
+    }
+    
+    public String MBTI() {
+        System.out.println("Welcome to 21Green's Climate Change MBTI Assignment!");
+        Scanner userInput = new Scanner(System.in);
+        int userMBTI = userInput.nextInt();
+        
+        switch (userMBTI){
+            
+            case 1:
+                
+            case 2:
+                
+            case 3:
+                
+            case 4:
+                
+            case 5:
+                
+            case 6:
+                
+            case 7:
+                
+            case 8:
+                
+            case 9:
+                
+            case 10:
+                
+            case 11:
+                
+            case 12:
+                
+            case 13:
+                
+            case 14:
+                
+            case 15:
+                
+            case 16:
+                
+            default:
+                
+            
+        }
+        
+        stringTest
+        
+        switch (stringTest){
+            
+        }
+        
+    }
+    
+    
+}
