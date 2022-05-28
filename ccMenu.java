b2632d3fb58cd00ffefc6967d5ff032c7ab9717c
Hyroqi <hiroq.i@outlook.com>
2022-05-28 16:54:52 +0800

    99% done

--- ccMenu.java
+++ ccMenu.java
@@ -21,7 +21,7 @@ public class ccMenu
 		while(!exit)
 		{
 			int usrInput = getInput();
-			perormAction(usrInput);
+			performAction(usrInput);
 		}
 	}
 
@@ -91,17 +91,99 @@ public class ccMenu
 
 	private void ccCauses()
 	{
-		
+		System.out.println("+-------------------------------------+");
+		System.out.println("|              Causes of              |");
+		System.out.println("|            Climate Change           |");
+		System.out.println("+-------------------------------------+");
+		System.out.println("1. Power Generation");
+		System.out.println("2. Manufacturing of Goods");
+		System.out.println("3. Deforestation");
+		System.out.println("4. Inefficient Usage of Transportation Methods");
+		System.out.println("0. Exit back to Infodesk");
+		switch(getInput())
+		{
+			case 1:
+				System.out.println("https://www.iea.org/commentaries/if-the-energy-sector-is-to-tackle-climate-change-it-must-also-think-about-water");
+				break;
+			case 2:
+				System.out.println("https://www.c2es.org/document/a-climate-of-change-manufacturing-must-rise-to-the-risks-and-opportunities-of-climate-change-2/#:~:text=Manufacturing%20operations%20that%20are%20most,as%20paper%20and%20chemicals%20operations.");
+				break;
+			case 3:
+				System.out.println("https://www.ucsusa.org/resources/tropical-deforestation-and-global-warming#:~:text=When%20forests%20are%20cut%20down,percent%20of%20global%20warming%20pollution.");
+				break;
+			case 4:
+				System.out.println("https://www.epa.gov/transportation-air-pollution-and-climate-change/carbon-pollution-transportation");
+				break;
+			case 0:
+				printHeader();
+				printMenu();
+				break;
+		}
 	}
 	
 	private void ccEffects()
 	{
-
+		System.out.println("+-------------------------------------+");
+		System.out.println("|              Effects of             |");
+		System.out.println("|            Climate Change           |");
+		System.out.println("+-------------------------------------+");
+		System.out.println("1. Hotter Temperatures");
+		System.out.println("2. Loss of Species");
+		System.out.println("3. Increased Severity of Natural Disasters");
+		System.out.println("4. A Warming, Rising Ocean");
+		System.out.println("0. Exit back to Infodesk");
+		switch(getInput())
+		{
+			case 1:
+				System.out.println("https://www.climate.gov/news-features/understanding-climate/climate-change-global-temperature#:~:text=According%20to%20NOAA%27s%202020%20Annual,more%20than%20twice%20that%20rate.");
+				break;
+			case 2:
+				System.out.println("https://www.biologicaldiversity.org/campaigns/global_warming_and_endangered_species/index.html#:~:text=Global%20warming%20is%20projected%20to,human%20societies%20across%20the%20globe.");
+				break;
+			case 3:
+				System.out.println("https://www.usgs.gov/faqs/how-can-climate-change-affect-natural-disasters");
+				break;
+			case 4:
+				System.out.println("https://climate.nasa.gov/vital-signs/sea-level/#:~:text=Global%20sea%20levels%20are%20rising,of%20seawater%20as%20it%20warms.");
+				break;
+			case 0:
+				printHeader();
+				printMenu();
+				break;
+		}
 	}
 
 	private void ccCombat()
 	{
-
+		System.out.println("+-------------------------------------+");
+		System.out.println("|        Methods used to combat       |");
+		System.out.println("|            Climate Change           |");
+		System.out.println("+-------------------------------------+");
+		System.out.println("1. Save Energy at Home");
+		System.out.println("2. Taking More Climate Friendly Methods of Transportation");
+		System.out.println("3. Reducing, Reusing and Recyling");
+		System.out.println("4. Changing Homes' sources of energy");
+		System.out.println("0. Exit back to Infodesk");
+		switch(getInput())
+		{
+			case 1:
+				System.out.println("https://19january2017snapshot.epa.gov/climatechange/what-you-can-do-home_.html");
+				
+				break;
+			case 2:
+				System.out.println("https://www.conserve-energy-future.com/transportation-alternatives-combat-climate-change.php");
+			break;
+			case 3:
+				System.out.println("https://www.canr.msu.edu/news/reduce_recycle_and_reuse_to_decrease_climate_change#:~:text=We%20can%20help%20protect%20our,Recycle%20waste%20products.");
+				break;
+			case 4:
+				System.out.println("https://www.sciencedirect.com/science/article/pii/0301421595001174#:~:text=Solar%20energy%20is%20one%20option,and%2032%25%20in%2050%20years.");
+				break;
+			case 0:
+				printHeader();
+				printMenu();
+				break;
+		}
 	}
 
 }
