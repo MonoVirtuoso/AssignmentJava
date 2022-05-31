3eed1c59ea682d99d778317f31bbff2e4a714dd4
MonoVirtuoso <muhammadaqheel@gmail.com>
2022-06-01 02:11:27 +0800

    Latest???

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[]args) 
     {
         AssignmentWork testMethod = new AssignmentWork();
-        testMethod.MBTI();
+        testMethod.Quiz();
     }
 
     
